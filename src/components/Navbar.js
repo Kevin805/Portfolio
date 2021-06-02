5a0c0221563eac91d2c7cb72be930df68e0243a2
Kevin Johnson <kevin.michael805@gmail.com>
2021-06-02 15:04:44 -0600

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import logo from '../coding.png';
+import logo2 from '../code.jpg';
+import '../App.css';
 
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/><img className="logo2" src={logo2} alt="logo2..."/></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
