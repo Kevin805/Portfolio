1fcfe5b3b1891715e1ec25188d11b1e092b72b90
Kevin Johnson <kevin.michael805@gmail.com>
2021-06-03 15:28:07 -0600

    Navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ import '../App.css';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
           <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/><img className="name" src={name} alt="name..."/><img className="logo2" src={logo2} alt="logo2..."/></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -20,10 +20,13 @@ export const Navbar = () => {
               <li className="nav-item">
                 <a className="nav-link" href="#">About</a>
               </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
             </ul>
           </div>
-        </div>
-      </nav>
+      </div>
+    </nav>
     )
 }
 
