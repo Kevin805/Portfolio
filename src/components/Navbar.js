8886ea88f2a8f9d6d57559297864a88a7cdf7a74
Kevin Johnson <kevin.michael805@gmail.com>
2021-06-08 12:57:27 -0600

    font awesome added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../logo.png";
 import '../App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
@@ -10,7 +12,7 @@ const Navbar = () => {
           <button className="navbar-toggler" type="button" data-toggle="collapse" 
           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
           aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <FontAwesomeIcon icon={faBars}/>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
