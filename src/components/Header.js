6bc52ac1668b3661db9cbec29fafcbab4af5d53d
Kevin Johnson <kevin.michael805@gmail.com>
2021-06-14 11:53:13 -0600

    react-typed added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
       <div className="header-wraper">
         <div className="main-info">
-            <h1>Web Development</h1>
+            <Typed
+              className="typed-text"
+              strings={["Web Development", "Web Design"]}
+              typeSpeed={35}
+              backSpeed={60}
+              loop
+              />
         </div>
       </div>
     )
