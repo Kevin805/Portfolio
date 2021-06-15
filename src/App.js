298e42ed37295b80c0111668d84eafc92fd54614
Kevin Johnson <kevin.michael805@gmail.com>
2021-06-15 15:46:43 -0600

    routing started.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Navbar from './components/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Particles from "react-particles-js";
-
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+ 
 function App() {
   return (
     <div>
+      <Router>
+        <Route path="/" component= {Portfolio} />
+      </Router>
       <Particles 
         params={{
           particles: {
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Portfolio() {
+    return (
+        <div>
+           <Header /> 
+        </div>
+    )
+}
+
+export default Portfolio;
