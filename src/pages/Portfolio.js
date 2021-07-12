ea499ddb3510fc6a6360c04bcc04572a5b39eaf7
Kevin Johnson <kevin.michael805@gmail.com>
2021-07-12 10:29:08 -0600

    updated portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,40 +1,36 @@
-import React from "react";
-import logo from "../logo.png";
 import '../App.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-
+import Navbar from '../Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from '../Header';
+import Particles from "react-particles-js";
+ 
 function Portfolio() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-          <a className="navbar-brand"><img className="logo" src={logo} alt="logo..."/>
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" 
-          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-          aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars}/>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                 <Link to="/Portfolio">Portfolio</Link> 
-              </li>
-              <li className="nav-item">
-                 <Link to="/About">About</Link> 
-              </li>
-              <li className="nav-item">
-                 <Link to="/Contact">Contact</Link> 
-              </li>
-              <li className="nav-item">
-                 <Link to="/Home">Home</Link> 
-              </li>
-            </ul>
-          </div>
-      </div>
-    </nav>
-    )
+  return (
+    <div>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable:true,
+                value_area: 800,
+              }
+            },
+            shape: {
+              type: "triangle",
+              stroke: {
+                width: 2.2,
+                color: "#f9ab"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </div>
+  );
 }
 
 export default Portfolio;
