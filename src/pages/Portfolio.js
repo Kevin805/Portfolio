01ff79a510be9bee6c45d641f958cb6a13db4141
Kevin Johnson <kevin.michael805@gmail.com>
2021-07-26 11:59:19 -0600

    changed reverted

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,3 +4,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from '../Header';
 import Particles from "react-particles-js";
  
+function Portfolio() {
+  return (
+    <div>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable:true,
+                value_area: 800,
+              }
+            },
+            shape: {
+              type: "triangle",
+              stroke: {
+                width: 2.2,
+                color: "#f9ab"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default Portfolio;
