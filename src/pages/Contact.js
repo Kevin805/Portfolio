45348b72c350d745d23a9937a802366af00fd938
Kevin Johnson <kevin.michael805@gmail.com>
2021-07-26 12:01:01 -0600

    Title change

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                 <Link to="/About">Portfolio</Link> 
+                 <Link to="/About">Work</Link> 
               </li>
               <li className="nav-item">
                  <Link to="/About">About</Link> 
