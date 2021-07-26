7a474343a94e9046986f0eb70d56abe190dbe088
Kevin Johnson <kevin.michael805@gmail.com>
2021-07-26 12:00:28 -0600

    Title change

--- src/Navbar.js
+++ src/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                 <Link to="/Portfolio">Portfolio</Link> 
+                 <Link to="/Portfolio">Work</Link> 
               </li>
               <li className="nav-item">
                  <Link to="/About">About</Link> 
